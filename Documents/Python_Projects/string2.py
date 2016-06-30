dc1c02b7eee6e17cb4a98f779f8beac64aee6543
kunalkhadalia04 <kunalkhadalia04@gmail.com>
2016-07-01 02:23:06 +0530

    sample

---
+++ Documents/Python_Projects/string2.py
@@ -0,0 +1,17 @@
+var1 = "ada"
+var2 = "lovelace"
+var3 =  10
+var4 = var1 + var2
+fox = " the \tquick \n\tbrown fox jumps over the lazy     dog  "
+print(var4)
+print(var1.upper() + "this is var1 in upper")
+print(var2.lower() + " \tthis is var2 in lower")
+print(var4.title() + " \tthis is var4 with a tab")
+#print(var3 + "this is value")
+print(fox)
+fox.rstrip()
+print(fox)
+sample1 = "    ada    " 
+sample1.rstrip()
+sample1 = sample1.rstrip()
+print(sample1 +"lovelace")
